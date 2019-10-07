2ffff3739a3cd3b091949a7a400b3fff1a6118a1
welton augusto <weltonarsantos@gmail.com>
2019-10-07 09:29:24 -0300

    1. Adição de método para seleção de features pela variância (filter_selection.py).

---
+++ projeto/utils/filter_selection.py
@@ -0,0 +1,26 @@
+from sys import argv, exit
+from utils import carregar_base
+from sklearn.feature_selection import VarianceThreshold, chi2
+
+def variance_threshold(dados):
+
+	# Removendo coluna de resultados.
+	dados = dados.drop(columns = "Result")
+
+	# Aplicando seletor de características.
+	seletor = VarianceThreshold()
+	caracteristicas = seletor.fit_transform(dados)
+	print(caracteristicas)
+
+def kbest(dados):
+
+	# Removendo coluna de resultados.
+	dados = dados.drop(columns = "Result")
+	# Trocando -1 (não phishing) por 2.
+	dados.replace(-1, 2)
+
+
+if __name__=='__main__':
+
+	dados = carregar_base(argv[1])
+	variance_threshold(dados)
