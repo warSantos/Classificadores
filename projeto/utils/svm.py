ecce836d5c03ecf52cc69d7bdabe04076c8d88b2
welton augusto <weltonarsantos@gmail.com>
2019-10-08 16:46:19 -0300

    1. Adição de feature selection com SequentialFeatureSelector.

--- projeto/utils/svm.py
+++ projeto/utils/svm.py
@@ -1,16 +1,17 @@
 from sys import argv
-from utils import carregar_base, seleciona_variaveis_RNE, validacao_cruzada
+from utils import carregar_base, seleciona_variaveis_SFS, validacao_cruzada
 from sklearn.svm import SVC
 from sklearn.feature_selection import SelectFromModel
+from sklearn.ensemble import RandomForestClassifier
 
 def busca_melhores_features(model, dados):
     print('\nBuscando a melhor seleção de variavel utilizando recursive feature elimination para RFC')
     melhor_score = 0.0
     melhor_i = 30
     for i in range(0, 30):
-        print('\nITERAÇÃO ',i,'------------------------------------------------------------------------')
+        print('\nITERAÇÃO ',i,'-'*50)
 
-        X, Y, features_excuidas = seleciona_variaveis_RNE(model, dados, 30-i)
+        X, Y, features_excuidas = seleciona_variaveis_SFS(model, dados, 30-i)
         atual_score = validacao_cruzada(X, Y, model)
         print("Acurácia: ", atual_score, "| Número de Features: ", 30-i, " --- ", X.shape)
         if(atual_score > melhor_score):
@@ -18,11 +19,11 @@ def busca_melhores_features(model, dados):
             melhor_i = i
             melhores_exclusoes = features_excuidas
 
-
     return melhor_score, melhor_i, melhores_exclusoes
 
 if __name__=='__main__':
     base = carregar_base(argv[1])
-    svm_modelo = SVC(C=1, kernel='linear')
+    #svm_modelo = SVC(C=1, gamma='scale')
+    svm_modelo = RandomForestClassifier(n_estimators = 100, criterion = 'entropy')
     score, num_variaveis = busca_melhores_features(svm_modelo, base)
     print("\n\nMelhor acurácia: ", score, 'Número de váriveis', 30 - num_variaveis)
